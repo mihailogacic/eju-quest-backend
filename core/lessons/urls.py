5f70aadc3dd46e5264a862c942d95c0488dda672
Stefan <stefwolf22@gmail.com>
2025-03-18 18:19:18 +0100

    database change

--- core/lessons/urls.py
+++ core/lessons/urls.py
@@ -10,4 +10,6 @@ from . import views
 urlpatterns = [
     path('generate-new-lesson/', views.GenerateNewLessonView.as_view(),
          name='generate-new-lesson'),
+
+    path('add-questions/', views.SaveLessonContentView.as_view(), name='save-lesson')
 ]
